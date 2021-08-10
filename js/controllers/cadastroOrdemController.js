ffcbe831d353c8107f9ae01af0104dfec5c3de4a
Thiago Piffer <thiago.piffer@hotmail.com>
2021-08-09 21:06:12 -0300

    Limpeza de dados na tela ao criar uma ordem de servico

--- js/controllers/cadastroOrdemController.js
+++ js/controllers/cadastroOrdemController.js
@@ -1,6 +1,12 @@
-appModule.controller("cadastroOrdemController", function($scope, clienteService, ordemService) {
+appModule.controller("cadastroOrdemController", function($location, $scope, clienteService, ordemService) {
 
     $scope.exibirErro = false;
+    $scope.isClienteAvailable = false;
+
+    var ordemservico = {
+        "idCliente": undefined,
+        "itens": [] 
+    };
 
     $scope.pesquisar = function(keyCode) {
         if(keyCode === 13){
@@ -15,6 +21,7 @@ appModule.controller("cadastroOrdemController", function($scope, clienteService,
     $scope.enviar = function() {
         if(!isOrdemInvalida()){
             enviarOrdem();
+            $location.path("/home");
         }
     }
 
@@ -23,9 +30,6 @@ appModule.controller("cadastroOrdemController", function($scope, clienteService,
             $scope.cliente = response.data;
             ordemservico.idCliente = $scope.cliente.id; //CONSTRUÇAO OBJ ORDEM: SETANDO ID DO CLIENTE
             $scope.cliente.endereco = toStringEndereco($scope.cliente.endereco);
-            
-            console.log($scope.cliente);
-            console.log(ordemservico);
             $scope.isClienteAvailable = true;
         }, function(err) {
             console.log(err) //TRATAR ERRO DE REQUISICAO EXIBINDO MENSAGEM NA TELA
@@ -37,28 +41,23 @@ appModule.controller("cadastroOrdemController", function($scope, clienteService,
         item.orcamento = 0.00;
         ordemservico.itens.push(angular.copy(item));
         delete($scope.item);
-
         $scope.ordemservico = ordemservico;
-        console.log(ordemservico);
     }
 
     var enviarOrdem = function() {
         ordemService.insertOrdem(ordemservico).then(function(response) {
             console.log(response);
+            limparTela();
         }, function(err) {
             console.log(err);
         })
     }
 
+    // UTILS
+
     var toStringEndereco = function(enderecoCliente) {
         return `${enderecoCliente.logradouro}, número ${enderecoCliente.numero}, bairro ${enderecoCliente.bairro}, ${enderecoCliente.cidade}, ${enderecoCliente.estado}`;
     }
-
-    var ordemservico = {
-        "idCliente": undefined,
-        "itens": [] 
-    };
-
 
     var isOrdemInvalida = function() {
         if(ordemservico.idCliente === undefined || ordemservico.itens.length === 0){
@@ -72,6 +71,22 @@ appModule.controller("cadastroOrdemController", function($scope, clienteService,
         $scope.mensagemErro = message;
         $scope.exibirErro = true;
         return true;
+    }
+
+    var resetOrdemServico = function() {
+        ordemservico = {
+            "idCliente": undefined,
+            "itens": [] 
+        };
+    }
+
+    var limparTela = function(){
+        delete($scope.ordemservico);
+        delete($scope.cliente);
+        delete($scope.pesquisa);
+        resetOrdemServico();
+        $scope.exibirErro = false;
+        $scope.isClienteAvailable = false;
     }
 
 });
